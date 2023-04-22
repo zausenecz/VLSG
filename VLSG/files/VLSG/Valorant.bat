2e39d86d3a8be5cfe4659a20b8382c96c56960aa
zausenecz <93251054+zausenecz@users.noreply.github.com>
2023-04-22 17:19:13 +0300

    Delete VLSG directory

--- VLSG/files/VLSG/Valorant.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-copy /y "%UserProfile%\Documents\VLSG\Packs\*.*" "C:\Riot Games\VALORANT\live\ShooterGame\Content\Paks"
-
-start "" "C:\Riot Games\Riot Client\RiotClientServices.exe" --launch-product=valorant --launch-patchline=live"
--- VLSG/script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Do you want to continue?
-pause
-xcopy /s /y "files/VLSG" "%UserProfile%\Documents\VLSG\"
-xcopy /s /y "files/Label" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\VLSG\"
