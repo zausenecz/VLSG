904b35801581c6cdef381e0888262f9321dbd70e
zausenecz <93251054+zausenecz@users.noreply.github.com>
2023-07-05 18:33:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,5 @@ var lngMain = document.getElementById('lngMain');
 var lngVoice = document.getElementById('lngVoice');
 
 function download() {
-    if (lngMain.options[lngMain.selectedIndex].value == "en" && lngVoice.options[lngVoice.selectedIndex].value == "jp")
-        window.open("https://github.com/zausenecz/VLSG/raw/main/Packs/en-jp.zip");
-    else
-    if (lngMain.options[lngMain.selectedIndex].value == "en" && lngVoice.options[lngVoice.selectedIndex].value == "ru")
-        window.open("https://github.com/zausenecz/VLSG/raw/main/Packs/en-ru.zip");
-    else
-        alert("Error ._. Contact with developer")
+        window.open("https://github.com/zausenecz/VLSG/raw/main/Packs/vlsg.zip");
 }
